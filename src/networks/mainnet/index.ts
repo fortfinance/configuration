f3c797aa96807845517c9ca01c58f8224f84a18e
Michael Francisco <mike.oostvoort@gmail.com>
2022-04-01 09:34:24 +0800

    feat(tokens): add aTokens from aave

--- src/networks/mainnet/index.ts
+++ src/networks/mainnet/index.ts
@@ -5,6 +5,7 @@ import uniswap_v2_tokens from "./tokens/uniswap_v2";
 import one_inch from "./platforms/one_inch";
 import fort from "./platforms/fort";
 import compound from "./platforms/compound";
+import aave_tokens from "./tokens/aave";
 
 const mainnet = {
     platforms: {
@@ -16,7 +17,8 @@ const mainnet = {
         ...regular_tokens,
         ...compound_tokens,
         ...uniswap_v2_tokens,
-        ...yearn_tokens
+        ...yearn_tokens,
+        ...aave_tokens
     },
 }
 
---
+++ src/networks/mainnet/tokens/aave.ts
@@ -0,0 +1,276 @@
+const aave_tokens = {
+    "aUSDT": {
+        "symbol": "aUSDT",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 6,
+        "address": "0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811"
+    },
+    "aWBTC": {
+        "symbol": "aWBTC",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 8,
+        "address": "0x9ff58f4fFB29fA2266Ab25e75e2A8b3503311656"
+    },
+    "aWETH": {
+        "symbol": "aWETH",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x030bA81f1c18d280636F32af80b9AAd02Cf0854e"
+    },
+    "aYFI": {
+        "symbol": "aYFI",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x5165d24277cD063F5ac44Efd447B27025e888f37"
+    },
+    "aZRX": {
+        "symbol": "aZRX",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xDf7FF54aAcAcbFf42dfe29DD6144A69b629f8C9e"
+    },
+    "aUNI": {
+        "symbol": "aUNI",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xB9D7CB55f463405CDfBe4E90a6D2Df01C2B92BF1"
+    },
+    "aAAVE": {
+        "symbol": "aAAVE",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xFFC97d72E13E01096502Cb8Eb52dEe56f74DAD7B"
+    },
+    "aBAT": {
+        "symbol": "aBAT",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x05Ec93c0365baAeAbF7AefFb0972ea7ECdD39CF1"
+    },
+    "aBUSD": {
+        "symbol": "aBUSD",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xA361718326c15715591c299427c62086F69923D9"
+    },
+    "aDAI": {
+        "symbol": "aDAI",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x028171bCA77440897B824Ca71D1c56caC55b68A3"
+    },
+    "aENJ": {
+        "symbol": "aENJ",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xaC6Df26a590F08dcC95D5a4705ae8abbc88509Ef"
+    },
+    "aKNC": {
+        "symbol": "aKNC",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x39C6b3e42d6A679d7D776778Fe880BC9487C2EDA"
+    },
+    "aLINK": {
+        "symbol": "aLINK",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xa06bC25B5805d5F8d82847D191Cb4Af5A3e873E0"
+    },
+    "aMANA": {
+        "symbol": "aMANA",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xa685a61171bb30d4072B338c80Cb7b2c865c873E"
+    },
+    "aMKR": {
+        "symbol": "aMKR",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xc713e5E149D5D0715DcD1c156a020976e7E56B88"
+    },
+    "aREN": {
+        "symbol": "aREN",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xCC12AbE4ff81c9378D670De1b57F8e0Dd228D77a"
+    },
+    "aSNX": {
+        "symbol": "aSNX",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x35f6B052C598d933D69A4EEC4D04c73A191fE6c2"
+    },
+    "aSUSD": {
+        "symbol": "aSUSD",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x6C5024Cd4F8A59110119C56f8933403A539555EB"
+    },
+    "aTUSD": {
+        "symbol": "aTUSD",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x101cc05f4A51C0319f570d5E146a8C625198e636"
+    },
+    "aUSDC": {
+        "symbol": "aUSDC",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 6,
+        "address": "0xBcca60bB61934080951369a648Fb03DF4F96263C"
+    },
+    "aCRV": {
+        "symbol": "aCRV",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x8dAE6Cb04688C62d939ed9B68d32Bc62e49970b1"
+    },
+    "aGUSD": {
+        "symbol": "aGUSD",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 2,
+        "address": "0xD37EE7e4f452C6638c96536e68090De8cBcdb583"
+    },
+    "aBAL": {
+        "symbol": "aBAL",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x272F97b7a56a387aE942350bBC7Df5700f8a4576"
+    },
+    "aXSUSHI": {
+        "symbol": "aXSUSHI",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xF256CC7847E919FAc9B808cC216cAc87CCF2f47a"
+    },
+    "aRENFIL": {
+        "symbol": "aRENFIL",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x514cd6756CCBe28772d4Cb81bC3156BA9d1744aa"
+    },
+    "aRAI": {
+        "symbol": "aRAI",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xc9BC48c72154ef3e5425641a3c747242112a46AF"
+    },
+    "aAMPL": {
+        "symbol": "aAMPL",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 9,
+        "address": "0x1E6bb68Acec8fefBD87D192bE09bb274170a0548"
+    },
+    "aUSDP": {
+        "symbol": "aUSDP",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x2e8F4bdbE3d47d7d7DE490437AeA9915D930F1A3"
+    },
+    "aDPI": {
+        "symbol": "aDPI",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x6F634c6135D2EBD550000ac92F494F9CB8183dAe"
+    },
+    "aFRAX": {
+        "symbol": "aFRAX",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0xd4937682df3C8aEF4FE912A96A74121C0829E664"
+    },
+    "aFEI": {
+        "symbol": "aFEI",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x683923dB55Fead99A79Fa01A27EeC3cB19679cC3"
+    },
+    "aSTETH": {
+        "symbol": "aSTETH",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x1982b2F5814301d4e9a8b0201555376e62F82428"
+    },
+    "aENS": {
+        "symbol": "aENS",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 18,
+        "address": "0x9a14e23A58edf4EFDcB360f68cd1b95ce2081a2F"
+    },
+    "aUST": {
+        "symbol": "aUST",
+        "platform": "aave",
+        "enabled": true,
+        "abi": "aToken",
+        "decimals": 6,
+        "address": "0xc2e2152647F4C26028482Efaf64b2Aa28779EFC4"
+    }
+}
+
+export default aave_tokens
