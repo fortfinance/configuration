74e88893176f5c62bf9e3b273b9ab50601d68c5e
Michael Francisco <mike.oostvoort@gmail.com>
2022-04-27 10:21:10 +0800

    chore: add more ctokens and add underlying token address

--- src/networks/mainnet/tokens/compound.ts
+++ src/networks/mainnet/tokens/compound.ts
@@ -1,28 +1,41 @@
 const compound_tokens = {
-    cZRX: {
-        address: '0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407',
-        symbol: 'cZRX',
+    cAAVE: {
+        address: '0xe65cdb6479bac1e22340e4e755fae7e509ecd06c',
+        symbol: 'cAAVE',
         abi: "cToken",
-        name: "cZRX",
-        decimals: 18,
+        name: "cAAVE",
+        decimals: 8,
         platform: 'compound',
-        enabled: true
+        enabled: true,
+        underlying: "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"
     },
-    cUNI: {
-        address: '0x35a18000230da775cac24873d00ff85bccded550',
-        symbol: 'cUNI',
+    cBAT: {
+        address: '0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E',
+        symbol: 'cBAT',
         abi: "cToken",
-        name: "cUNI",
-        decimals: 18,
+        name: "cBAT",
+        decimals: 8,
         platform: 'compound',
-        enabled: true
+        enabled: true,
+        underlying: "0x0D8775F648430679A709E98d2b0Cb6250d2887EF",
     },
-    cWBTC2: {
-        address: '0xccf4429db6322d5c611ee964527d42e5d685dd6a',
-        symbol: 'cWBTC2',
+    cCOMP: {
+        address: '0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4',
+        symbol: 'cCOMP',
         abi: "cToken",
-        name: "cWBTC2",
-        decimals: 18,
+        name: "cCOMP",
+        decimals: 8,
+        platform: 'compound',
+        enabled: true,
+        underlying: "0xc00e94cb662c3520282e6f5717214004a7f26888"
+    },
+    cDAI: {
+        address: '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643',
+        symbol: 'cDAI',
+        abi: "cToken",
+        name: "cDAI",
+        decimals: 8,
+        underlying: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
         platform: 'compound',
         enabled: true
     },
@@ -31,26 +44,71 @@ const compound_tokens = {
         symbol: 'cETH',
         abi: "CETH",
         name: "cETH",
+        decimals:8,
+        platform: 'compound',
+        enabled: true
+    },
+    cFEI: {
+        address: '0x7713DD9Ca933848F6819F38B8352D9A15EA73F67',
+        symbol: 'cFEI',
+        abi: "cToken",
+        name: "Compound FEI USD",
+        decimals:8,
+        platform: 'compound',
+        enabled: true,
+        underlying: "0x956F47F50A910163D8BF957Cf5846D573E7f87CA"
+    },
+    cLINK: {
+        address: '0xface851a4921ce59e912d19329929ce6da6eb0c7',
+        symbol: 'cLINK',
+        abi: "cToken",
+        name: "cLINK",
+        decimals: 8,
+        platform: 'compound',
+        enabled: true,
+        underlying: "0x514910771af9ca656af840dff83e8264ecf986ca"
+
+    },
+    cMKR: {
+        address: "0x95b4eF2869eBD94BEb4eEE400a99824BF5DC325b",
+        symbol: "cMKR",
+        abi: "cToken",
+        name: "Compound Maker",
+        decimals:8,
+        platform: 'compound',
+        enabled: true,
+        underlying: "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2",
+
+    },
+    cSUSHI: {
+        address: '0x4b0181102a0112a2ef11abee5563bb4a3176c9d7',
+        symbol: 'cSUSHI',
+        abi: "cToken",
+        name: "cSUSHI",
+        decimals: 8,
+        platform: 'compound',
+        underlying: "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2",
+
+        enabled: true
+    },
+    cTUSD: {
+        address: '0x12392f67bdf24fae0af363c24ac620a2f67dad86',
+        symbol: 'cTUSD',
+        abi: "cToken",
+        name: "cTUSD",
+        underlying: "0x0000000000085d4780b73119b644ae5ecd22b376",
         decimals: 8,
         platform: 'compound',
         enabled: true
     },
-    cCOMP: {
-        address: '0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4',
-        symbol: 'cCOMP',
+    cUNI: {
+        address: '0x35a18000230da775cac24873d00ff85bccded550',
+        symbol: 'cUNI',
         abi: "cToken",
-        name: "cCOMP",
-        decimals: 18,
+        name: "cUNI",
+        decimals: 8,
         platform: 'compound',
-        enabled: true
-    },
-    cDAI: {
-        address: '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643',
-        symbol: 'cDAI',
-        abi: "cToken",
-        name: "cDAI",
-        decimals: 18,
-        platform: 'compound',
+        underlying: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
         enabled: true
     },
     cUSDC: {
@@ -58,7 +116,19 @@ const compound_tokens = {
         symbol: 'cUSDC',
         abi: "cToken",
         name: "cUSDC",
-        decimals: 18,
+        decimals: 8,
+        platform: 'compound',
+        enabled: true,
+        underlying: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+
+    },
+    cUSDCP: {
+        address: '0x041171993284df560249b57358f931d9eb7b925d',
+        symbol: 'cUSDP',
+        abi: "cToken",
+        name: "Compound Pax Dollar",
+        decimals: 8,
+        underlying: "0x8e870d67f660d95d5be530380d0ec0bd388289e1",
         platform: 'compound',
         enabled: true
     },
@@ -67,34 +137,18 @@ const compound_tokens = {
         symbol: 'cUSDT',
         abi: "cToken",
         name: "cUSDT",
-        decimals: 18,
+        decimals: 8,
         platform: 'compound',
-        enabled: true
+        enabled: true,
+        underlying: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
     },
-    cLINK: {
-        address: '0xface851a4921ce59e912d19329929ce6da6eb0c7',
-        symbol: 'cLINK',
+    cWBTC2: {
+        address: '0xccf4429db6322d5c611ee964527d42e5d685dd6a',
+        symbol: 'cWBTC2',
         abi: "cToken",
-        name: "cLINK",
-        decimals: 18,
-        platform: 'compound',
-        enabled: true
-    },
-    cAAVE: {
-        address: '0xe65cdb6479bac1e22340e4e755fae7e509ecd06c',
-        symbol: 'cAAVE',
-        abi: "cToken",
-        name: "cAAVE",
-        decimals: 18,
-        platform: 'compound',
-        enabled: true
-    },
-    cSUSHI: {
-        address: '0x4b0181102a0112a2ef11abee5563bb4a3176c9d7',
-        symbol: 'cSUSHI',
-        abi: "cToken",
-        name: "cSUSHI",
-        decimals: 18,
+        name: "cWBTC2",
+        decimals: 8,
+        underlying: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
         platform: 'compound',
         enabled: true
     },
@@ -103,25 +157,18 @@ const compound_tokens = {
         symbol: 'cYFI',
         abi: "cToken",
         name: "cYFI",
-        decimals: 18,
+        decimals: 8,
+        underlying: "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e",
         platform: 'compound',
         enabled: true
     },
-    cTUSD: {
-        address: '0x12392f67bdf24fae0af363c24ac620a2f67dad86',
-        symbol: 'cTUSD',
+    cZRX: {
+        address: '0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407',
+        symbol: 'cZRX',
         abi: "cToken",
-        name: "cTUSD",
-        decimals: 18,
-        platform: 'compound',
-        enabled: true
-    },
-    cBAT: {
-        address: '0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E',
-        symbol: 'cBAT',
-        abi: "cToken",
-        name: "cBAT",
-        decimals: 18,
+        underlying: "0xE41d2489571d322189246DaFA5ebDe1F4699F498",
+        name: "cZRX",
+        decimals: 8,
         platform: 'compound',
         enabled: true
     },
