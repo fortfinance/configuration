8a2059b83c2659dff60f6297370ea1828520edad
Michael Francisco <mike.oostvoort@gmail.com>
2022-05-04 08:21:18 +0800

    refactor config

--- src/networks/mainnet/platforms/compound.ts
+++ src/networks/mainnet/platforms/compound.ts
@@ -1,12 +1,14 @@
 const compound = {
     compound: {
-        Comptroller: {
-            address: '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B',
-            abi: "Comptroller"
-        },
-        PriceFeed: {
-            address: '0x6D2299C48a8dD07a872FDd0F8233924872Ad1071',
-            abi: "PriceFeed"
+        contracts: {
+            Comptroller: {
+                address: '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B',
+                abi: "Comptroller"
+            },
+            PriceFeed: {
+                address: '0x6D2299C48a8dD07a872FDd0F8233924872Ad1071',
+                abi: "PriceFeed"
+            }
         }
     }
 };
--- src/networks/mainnet/platforms/one_inch.ts
+++ src/networks/mainnet/platforms/one_inch.ts
@@ -1,8 +1,58 @@
+import regular_tokens from "../tokens/regular";
+
 const one_inch = {
     one_inch: {
-        OffchainOracle: {
-            address: '0x07D91f5fb9Bf7798734C3f606dB065549F6893bb',
-            abi: "OffchainOracle"
+        settings: {
+            supported_from_tokens: {
+                ...regular_tokens.UNI,
+                ...regular_tokens.USDT,
+                ...regular_tokens.WETH,
+                ...regular_tokens.USDC,
+                ...regular_tokens.DAI,
+                ...regular_tokens.COMP,
+                ...regular_tokens.LINK,
+                ...regular_tokens.AAVE,
+                ...regular_tokens.UST,
+                ...regular_tokens.CRV,
+                ...regular_tokens.WBTC,
+                ...regular_tokens.NFTX
+            },
+            supported_protocols: '' +
+                'SUSHI,' +
+                'AAVE_V2,' +
+                'UNISWAP_V2,' +
+                'DEFISWAP,' +
+                'UNISWAP_V3,' +
+                'DEFI_PLAZA,' +
+                'UNISWAP_V1,' +
+                'CURVE,' +
+                'LUASWAP,' +
+                'MINISWAP,' +
+                'SWERVE,' +
+                'DXSWAP,' +
+                'ST_ETH,' +
+                'BANCOR,' +
+                'KYBER_DMM,' +
+                'ONE_INCH_LP_1_1,' +
+                'LINKSWAP,' +
+                'SHIBASWAP,' +
+                'BALANCER,' +
+                'DODO,' +
+                'DODO_V2,' +
+                'BALANCER_V2,' +
+                'SMOOTHY_FINANCE,' +
+                'CURVE_V2,' +
+                'SADDLE,' +
+                'CURVE_V2_ETH_CVX,' +
+                'CURVE_V2_ETH_CRV,' +
+                'MOONISWAP,' +
+                'CURVE_V2_SPELL_2_ASSET'
+        },
+        contracts: {
+            OffchainOracle: {
+                address: '0x07D91f5fb9Bf7798734C3f606dB065549F6893bb',
+                abi: "OffchainOracle"
+            }
         }
     }
 };
